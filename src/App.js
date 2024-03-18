79c49ae0e0aa56256802b5dd0f90152993099a19
“Liron” <liron612@gmail.com>
2024-03-18 04:53:31 -0400

    cosmetics

--- src/App.js
+++ src/App.js
@@ -21,18 +21,17 @@ function App() {
                 </div>
             </div>
             <h3 className="mt-3">Shopping Cart</h3>
-                <div className="row mt-3">
-                    <div className="col-sm">
-                        <ExpenseList />
-                    </div>
+            <div className="row mt-3">
+                <div className="col-sm">
+                    <ExpenseList />
                 </div>
-                
+            </div>
             <h3 className="mt-3">Add Items</h3>
-                <div className="row mt-3">
-                    <div className="col-sm">
-                        <ItemSelected />
-                    </div>
+            <div className="row mt-3">
+                <div className="col-sm">
+                    <ItemSelected />
                 </div>
+            </div>
         </div>
     </AppProvider>
   );
